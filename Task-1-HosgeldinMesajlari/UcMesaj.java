2270dc4ce19fe6955bdcc7341552a7d7be328edb
250541623-arch <250541623@firat.edu.tr>
2025-10-23 16:16:10 +0300

    UcMesaj.java

---
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -0,0 +1,9 @@
+public class UcMesaj {
+    public static void main(String[] args)  {
+        System.out.println("Javaya Hoş Geldiniz");
+        System.out.println("Bilgisayar Bilimine HoşGeldiniz")
+        System.out.println("Proglamlama Eğlencelidir");
+        
+    }
+
+}
