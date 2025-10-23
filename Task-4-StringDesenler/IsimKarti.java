a69bda6af13be4481d39ed2675d6348f247ac9df
250541623-arch <250541623@firat.edu.tr>
2025-10-23 16:29:41 +0300

    IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,20 @@
+
+
+     public class İsimKarti {
+    public static void main(String[] args) {
+    String ad= "Mustafa";
+    String soyad="haccar";
+    String ogrno="250541623";
+    String bolum="yazilim Muh.";
+
+    System.out.println(",,,,,,,,,,,,,");
+    System.out.println("Ogrenci bilgileri");
+    System.out.println(",,,,,,,,,,,,,,,,,,,,");
+    System.out.println("Ad :" + ad);
+    System.out.println("Soyad  :"  +soyad);
+    System.out.println("No :" +ogrno);
+    System.out.println("BOlum :"+bolum);
+     System.out.println(",,,,,,,,,,");
+    }
+}
+
